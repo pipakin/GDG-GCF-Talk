8ab0dc01f157dd6bc45235956d35db265c6f078d
Philip Kin <philip@thekinfamily.com>
2017-10-16 17:18:53 -0500

    Fix?

--- js/plugins/GCF_Talk.js
+++ js/plugins/GCF_Talk.js
@@ -9,6 +9,33 @@
         }
         return ImageManager.oldLoadPicture(filename, hue);
     };
+
+    Bitmap.prototype._requestImage = function(url){
+        if(Bitmap._reuseImages.length !== 0){
+            this._image = Bitmap._reuseImages.pop();
+        }else{
+            this._image = new Image();
+        }
+
+        if (this._decodeAfterRequest && !this._loader) {
+            this._loader = ResourceHandler.createLoader(url, this._requestImage.bind(this, url), this._onError.bind(this));
+        }
+
+        this._image = new Image();
+        this._image.crossOrigin = "";
+        this._url = url;
+        this._loadingState = 'requesting';
+
+        if(!Decrypter.checkImgIgnore(url) && Decrypter.hasEncryptedImages) {
+            this._loadingState = 'decrypting';
+            Decrypter.decryptImg(url, this);
+        } else {
+            this._image.src = url;
+
+            this._image.addEventListener('load', this._loadListener = Bitmap.prototype._onLoad.bind(this));
+            this._image.addEventListener('error', this._errorListener = this._loader || Bitmap.prototype._onError.bind(this));
+        }
+    };
     var _Game_Interpreter_pluginCommand =
             Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function(command, args) {
